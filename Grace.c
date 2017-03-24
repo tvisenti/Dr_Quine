3ec3c55798dc8d13930a62dd8791dac8411284d5
tvisenti <tvisenti@student.42.fr>
2017-03-24 18:48:40 +0100

    [Grace]: Grace first push

---
+++ Grace.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#define NOT
+#define USED
+#define FUNCTION(x) int main(){FILE *fd = fopen("Grace_kid.c", "w+");fprintf(fd, "test\n");fclose(fd);}
+/*
+	This is a proper comment.
+*/
+FUNCTION(1);
