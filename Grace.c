a5c796750e108dc1150a8f882802b7ddbb6f53f0
tvisenti <tvisenti@student.42.fr>
2017-03-27 10:57:44 +0200

    [Grace]: Create a new file and write inside with only #define

--- Grace.c
+++ Grace.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #define NOT
 #define USED
-#define FUNCTION(x) int main(){FILE *fd = fopen("Grace_kid.c", "w+");fprintf(fd, "test\n");fclose(fd);}
+#define FUNCTION(x) int main(){FILE *fd = fopen(x,"w+");char*s="#include<stdio.h>%1$c#define NOT%1$c#define USED%1$c#define FUNCTION(x) int main(){FILE *fd = fopen(x,%2$cw+%2$c);char*s=%2$c%4$s%2$c;fprintf(fd,s,10,34,9,s);fclose(fd);}%1$c/*%1$c%3$cThis is a proper comment.%1$c*/%1$cFUNCTION(%2$cGrace_kid.c%2$c);%1$c";fprintf(fd,s,10,34,9,s);fclose(fd);}
 /*
 	This is a proper comment.
 */
-FUNCTION(1);
+FUNCTION("Grace_kid.c");
