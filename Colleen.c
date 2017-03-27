4355bd8b8d3ab17b022e6a31a968db168139771f
tvisenti <tvisenti@student.42.fr>
2017-03-27 10:35:03 +0200

    [Colleen]: Add reference to printf

--- Colleen.c
+++ Colleen.c
@@ -8,4 +8,4 @@ int main(){
 	Comment inside the func.
 */
 foo();
-char*s="#include<stdio.h>%c/*%c%cComment outside the func.%c*/%cvoid foo(){}%cint main(){%c/*%c%cComment inside the func.%c*/%cfoo();%cchar*s=%c%s%c;printf(s,10,10,9,10,10,10,10,10,9,10,10,10,34,s,34,10);}%c";printf(s,10,10,9,10,10,10,10,10,9,10,10,10,34,s,34,10);}
+char*s="#include<stdio.h>%1$c/*%1$c%2$cComment outside the func.%1$c*/%1$cvoid foo(){}%1$cint main(){%1$c/*%1$c%2$cComment inside the func.%1$c*/%1$cfoo();%1$cchar*s=%3$c%s%3$c;printf(s,10,9,34,s);}%1$c";printf(s,10,9,34,s);}
