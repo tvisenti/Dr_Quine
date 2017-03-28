843d7fbb114a24aa55b3f5b70c831359f0baf6cd
tvisenti <tvisenti@student.42.fr>
2017-03-28 18:19:27 +0200

    [Grace]: Remove echo

--- PHP/Grace.php
+++ PHP/Grace.php
@@ -2,8 +2,8 @@
 //
 //	Comment
 //
-define("STR", $str = "<?php%c//%c//%cComment%c//%cdefine(%cSTR%c, %cstr = %c%s%c);%cdefine(%cOPEN%c, %cfd = fopen(%cGrace_kid.php%c, %cw+%c));%cdefine(%cSTART%c, fprintf(OPEN, STR, 10,10,9,10,10,34,34,36,34,%cstr,34,10,34,34,36,34,34,34,34,10,34,34,36,10,10,10));%cecho START;%c?>%c");
+define("STR", $str = "<?php%c//%c//%cComment%c//%cdefine(%cSTR%c, %cstr = %c%s%c);%cdefine(%cOPEN%c, %cfd = fopen(%cGrace_kid.php%c, %cw+%c));%cdefine(%cSTART%c, fprintf(OPEN, STR, 10,10,9,10,10,34,34,36,34,%cstr,34,10,34,34,36,34,34,34,34,10,34,34,36,10,10,10));%cSTART;%c?>%c");
 define("OPEN", $fd = fopen("Grace_kid.php", "w+"));
 define("START", fprintf(OPEN, STR, 10,10,9,10,10,34,34,36,34,$str,34,10,34,34,36,34,34,34,34,10,34,34,36,10,10,10));
-echo START;
+START;
 ?>
